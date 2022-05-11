913858e51cc23b0ff294f08e350d11e1adcf0dd6
Charlotte Entwisle-Phillips <105251205+C20078@users.noreply.github.com>
2022-05-11 00:38:20 +0000

    learning how to do this

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print ("it worked")
+# unit standard as91883 - as91884. Created by Charlotte Entwisle-Phillips, Started on 10/5/2022. My goal is to make a te reo quiz.
+#https://replit.com/@Charlotte20078/test#main.py
+
+
+
+def question():
+  print ("is a quiz") 
+question()
