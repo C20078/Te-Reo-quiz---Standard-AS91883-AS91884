bc4ab3854e4b63e489d3520586d85d8bf0ae4867
Charlotte Entwisle-Phillips <105251205+C20078@users.noreply.github.com>
2022-05-12 06:54:55 +0000

    the quiz currently works well :)

--- main.py
+++ main.py
@@ -3,28 +3,31 @@
 
 def q1():
   print ("Question 1")
-  colour = input ("Whero is what color? Red / Green /  Orange / Blue = ".lower())
+  colour = input ("Whero is what color? Red / Green /  Orange / Blue = ")
+  colour = colour.lower()
   if colour == "red":
-    print ("correct")
+    print ("Correct")
     q2()
   else:
-    print ("wrong")
+    print ("Wrong")
     wrong()
 
 
 def q2():
-  print ("this is question 2") 
+  print ("Question 2") 
   dance = input ("What is the traditional maori dance called? Pukana / Haka / Kabuki / Taki = ")
+  dance = dance.lower()
   if dance == "haka":
     print ("Correct")
     q3()
   else:
-    print ("wrong")
+    print ("Wrong")
     wrong()
   
 def q3():
-  print ("This is question 3") 
+  print ("Question 3") 
   song = input("Fill in the blank for the song - One day a ____ went swimming in the moana. Taniwha / Mokopuna / Tuna = ")
+  song = song.lower()
   if song == "taniwha":
     print ("Correct")
     q4()
@@ -33,45 +36,50 @@ def q3():
     wrong()
   
 def q4():
-  print ("This is question 4") 
+  print ("Question 4") 
   ocean = input("What is the maori word for ocean?Wai / Moana = ")
+  ocean = ocean.lower()
   if ocean == "moana":
     print ("Correct")
     q5()
   else:
-    print ("wrong")
+    print ("Wrong")
     wrong()
   
 def q5():
-  print ("This is question 5") 
+  print ("Question 5") 
   word = input ("Niu Tireni is the Māori word for New Zealand. False / True = ")
+  word = word.lower()
   if word == "false":
     print ("Correct")
     correct()
   else:
-    print ("wrong")
+    print ("Wrong")
     wrong()
     
   
 def q6():
-  print ("This is question 6")
+  print ("Question 6")
   store = input(" What would be stored in a Paataka? Pukapuka / Kai / Waka = ")
+  store = store.lower()
   if input == "kai":
-    print ("correct")
+    print ("Correct")
     correct()
   else:
-    print ("wrong")
+    print ("Wrong")
     wrong()
 
 def wrong():
-  print ("u suck")
+  print ("Sorry that answer was wrong")
 
 def correct():
-  print ("good job")
+  print ("You got them all correct! Ka pai")
+  print ("(っ◔◡◔)っ ♥ your amazing ♥")
 
-print ("hi this quiz")
+print ("Te reo maori quiz - created by Charlotte ")
+print ("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+user = input ("Hello, What is your name? ") 
+print ("Ki ora, " + user + " this is a quiz created to test your maori language skills and New Zealand knowledge. There a 6 questions, Good luck! ")
 
-user = input ("what is name ")
-print ("hi " + user)
 q1()
 
