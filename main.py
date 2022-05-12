5e6132f5f23b86de9d4b0d54de3ae83e0650ba8c
Charlotte Entwisle-Phillips <105251205+C20078@users.noreply.github.com>
2022-05-12 04:48:56 +0000

    adding questions to code, currently working from home

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 # unit standard as91883 - as91884. Created by Charlotte Entwisle-Phillips, Started on 10/5/2022. My goal is to make a te reo quiz.
-#https://replit.com/@Charlotte20078/test#main.py
+#https://replit.com/@Charlotte20078/test#main.py......https://replit.com/@reiman/Python-Challenge-The-Quiz#main.py
 
+def q1():
+  print ("Question 1")
+  colour = input ("Whero is what color? Red / Green /  Orange / Blue")
+  if colour ==
 
+def q2():
+  print ("this is question 2, What is the traditional maori dance called? Pukana / Haka / Kabuki / Taki")
+  
+def q3():
+  print ("This is question 3, Fill in the blank for the song - One day a ____ went swimming in the moana. Taniwha / Mokopuna / Tuna")
+  
+def q4():
+  print ("This is question 4, What is the maori word for ocean?Wai / Moana")
+  
+def q5():
 
-def question():
-  print ("is a quiz") 
-question()
+  print ("This is question 5, Niu Tireni is the Māori word for New Zealand. False / True")
+  
+def q6():
+  print ("This is question 6, What would be stored in a Paataka? Pukapuka / Kai / Waka")
+
+print ("hi this quiz")
+
+user = input ("what is name")
+print ("hi " + user)
+q1()
