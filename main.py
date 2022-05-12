a6d20869c0ea784edfc14fd154a98ab1ce9156fa
Charlotte Entwisle-Phillips <105251205+C20078@users.noreply.github.com>
2022-05-12 05:14:58 +0000

    i keep forgeting to put the == when i type the if stament

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 def q1():
   print ("Question 1")
-  colour = input ("Whero is what color? Red / Green /  Orange / Blue =")
+  colour = input ("Whero is what color? Red / Green /  Orange / Blue = ")
   if colour == "Red":
     print ("correct")
     q2()
@@ -13,17 +13,44 @@ def q1():
 
 
 def q2():
-  print ("this is question 2, What is the traditional maori dance called? Pukana / Haka / Kabuki / Taki")
+  print ("this is question 2") 
+  dance = input ("What is the traditional maori dance called? Pukana / Haka / Kabuki / Taki = ")
+  if dance == "haka":
+    print ("Correct")
+    q3()
+  else:
+    print ("wrong")
+    q3()
   
 def q3():
-  print ("This is question 3, Fill in the blank for the song - One day a ____ went swimming in the moana. Taniwha / Mokopuna / Tuna")
+  print ("This is question 3") 
+  song = input("Fill in the blank for the song - One day a ____ went swimming in the moana. Taniwha / Mokopuna / Tuna = ")
+  if song == "taniwha":
+    print ("Correct")
+    q4()
+  else:
+    print ("wrong")
+    q4()
   
 def q4():
-  print ("This is question 4, What is the maori word for ocean?Wai / Moana")
+  print ("This is question 4") 
+  ocean = input("What is the maori word for ocean?Wai / Moana = ")
+  if ocean == "moana":
+    print ("Correct")
+    q5()
+  else:
+    print ("wrong")
+    q5()
   
 def q5():
-
-  print ("This is question 5, Niu Tireni is the Māori word for New Zealand. False / True")
+  print ("This is question 5") 
+  word = input ("Niu Tireni is the Māori word for New Zealand. False / True = ")
+  if word == "false":
+    print ("Correct")
+    q4()
+  else:
+    print ("wrong")
+    q4()
   
 def q6():
   print ("This is question 6, What would be stored in a Paataka? Pukapuka / Kai / Waka")
