04d860ee24876f671569a7184e28c5abb5470a44
Laurence Earl <84546222+learlfraser@users.noreply.github.com>
2022-05-16 22:16:16 +0000

    Worked on it today, Solved issues with overlapping code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # unit standard as91883 - as91884. Created by Charlotte Entwisle-Phillips, Started on 10/5/2022. My goal is to make a te reo quiz.
 #https://replit.com/@Charlotte20078/test#main.py......https://replit.com/@reiman/Python-Challenge-The-Quiz#main.py
 
+#purpose of code is to create a quiz while being funtional and good/easy to understand
+
+#this part of code should send them to q2 or wrong depending on input v6
 def q1():
   print ("Question 1")
   colour = input ("Whero is what color? Red / Green /  Orange / Blue = ")
@@ -12,10 +15,10 @@ def q1():
     print ("Wrong")
     wrong()
 
-
+#ask question 2, this part of code should send them to q3 or wrong depending on input v3
 def q2():
   print ("Question 2") 
-  dance = input ("What is the traditional maori dance called? Pukana / Haka / Kabuki / Taki = ")
+  dance = input ("What is the traditional maori dance called? Pukana / Haka / Kabuki / Taki =")
   dance = dance.lower()
   if dance == "haka":
     print ("Correct")
@@ -23,21 +26,23 @@ def q2():
   else:
     print ("Wrong")
     wrong()
-  
+
+#ask question 3, this part of code should send them to q4 or wrong depending on input v3
 def q3():
   print ("Question 3") 
-  song = input("Fill in the blank for the song - One day a ____ went swimming in the moana. Taniwha / Mokopuna / Tuna = ")
+  song = input("Fill in the blank for the song - One day a ____ went swimming in the moana. Taniwha / Mokopuna / Tuna = \n")
   song = song.lower()
   if song == "taniwha":
     print ("Correct")
     q4()
   else:
-    print ("wrong")
+    print ("Wrong")
     wrong()
-  
+    
+#ask question 4, this part of code should send them to q5 or wrong depending on input  v3
 def q4():
   print ("Question 4") 
-  ocean = input("What is the maori word for ocean?Wai / Moana = ")
+  ocean = input("What is the maori word for ocean? Wai / Moana = ")
   ocean = ocean.lower()
   if ocean == "moana":
     print ("Correct")
@@ -45,7 +50,8 @@ def q4():
   else:
     print ("Wrong")
     wrong()
-  
+
+#ask question 5, this part of code should send them to q6 or wrong depending on input v3
 def q5():
   print ("Question 5") 
   word = input ("Niu Tireni is the Māori word for New Zealand. False / True = ")
@@ -57,7 +63,7 @@ def q5():
     print ("Wrong")
     wrong()
     
-  
+#ask question 6, this part of code should send them to correct or wrong depending on input v4
 def q6():
   print ("Question 6")
   store = input(" What would be stored in a Paataka? Pukapuka / Kai / Waka = ")
@@ -68,18 +74,20 @@ def q6():
   else:
     print ("Wrong")
     wrong()
-
+#if a answer is wrong v2
 def wrong():
-  print ("Sorry that answer was wrong")
+  print ("Sorry that answer was wrong, please try to answer this quiz again. Good luck!!")
 
+#if all answers are correct v3
 def correct():
   print ("You got them all correct! Ka pai")
-  print ("(っ◔◡◔)っ ♥ your amazing ♥")
+  print ("(っ◔◡◔)っ ♥ you're amazing ♥")
 
+#this is the intro, welcome the user explain the quiz. this is version3. dates edited/made 9/5/22 11/5/22 12/5/22
 print ("Te reo maori quiz - created by Charlotte ")
 print ("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
 user = input ("Hello, What is your name? ") 
 print ("Ki ora, " + user + " this is a quiz created to test your maori language skills and New Zealand knowledge. There a 6 questions, Good luck! ")
-
+#ask question 1
 q1()
 
